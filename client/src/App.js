1781cd555ae5ed4511c06b766501ab337624e772
voidSatisfaction <lourie@naver.com>
2017-06-29 01:13:14 +0900

    chore: implement material ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,4 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-
-import Home from './components/page/Home';
-import Page1 from './components/page/Page1';
-import Page2 from './components/page/Page2';
-import MyPage from './components/page/MyPage';
-import Login from './components/page/Login';
-import Search from './components/page/Search';
-import NotFound from './components/page/NotFound';
 
 import Header from './components/organism/Header';
 
@@ -15,20 +6,10 @@ export default class App extends Component {
   state = {  }
   render() {
     return (
-      <Router>
-        <div>
-          <Header />
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/page1/:username" component={Page1} />
-            <Route path="/page2" component={Page2} />
-            <Route path="/login" component={Login} />
-            <Route path="/me" component={MyPage} />
-            <Route path="/search" component={Search} />
-            <Route component={NotFound} />
-          </Switch>
-        </div>
-      </Router>
+      <div>
+        <Header />
+        <h1>Hello world!</h1>
+      </div>
     );
   }
 }
--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+injectTapEventPlugin();
+
+ReactDOM.render(
+  <MuiThemeProvider>
+    <App />
+  </MuiThemeProvider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
