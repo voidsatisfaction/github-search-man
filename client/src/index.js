eadb44c7b942645b6c5d1a03a41afc9d413fc803
voidSatisfaction <lourie@naver.com>
2017-06-29 22:34:06 +0900

    feat: add redux and redux debug tool

--- client/src/index.js
+++ client/src/index.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import storeOfApp from './ducks'
+import rootReducer from './ducks'
 import './vendor/css/grid.css';
 import './vendor/css/font.css';
 import './index.css';
 
 injectTapEventPlugin();
 
-const store = createStore(storeOfApp);
+const store = createStore(
+  rootReducer,
+  applyMiddleware(thunk),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
   <MuiThemeProvider>
