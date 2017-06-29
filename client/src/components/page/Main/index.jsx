82d83a3eb2cf0cb2ef1e45a40fae4a6c75b58a7e
voidSatisfaction <lourie@naver.com>
2017-06-29 13:59:36 +0900

    feat: add lists layout on main

--- client/src/components/page/Main/index.jsx
+++ client/src/components/page/Main/index.jsx
@@ -1,16 +1,31 @@
 import React, { Component } from 'react';
 
+import TextField from 'material-ui/TextField';
+
+import ListRepos from '../../organism/ListRepos';
 import MainWrapper from '../../molecule/MainWrapper';
 import Panel from '../../molecule/Panel';
+
+const listLegends = ['id','name','language','stars','updated'];
 
 const Main = (props) => (
   <MainWrapper>
     <Panel col="1-6" />
     <Panel col="3-6">
       <h1>Repository Search</h1>
+      <TextField 
+        fullWidth
+        hintText="Input Repositry keyword!"
+        style={{
+          margin: '20px'
+        }}
+      />
+      <ListRepos
+        legends={listLegends}
+      />
     </Panel>
     <Panel col="1-6">
-      <h1>rightside</h1>
+      <h1>watched</h1>
     </Panel>
     <Panel col="1-6" />
   </MainWrapper>
