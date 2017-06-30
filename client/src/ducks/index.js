438a7606a226ece5244dda82cf8142f6f242b6e5
voidSatisfaction <lourie@naver.com>
2017-07-01 01:01:34 +0900

    fix: delete my watching repos

--- client/src/ducks/index.js
+++ client/src/ducks/index.js
@@ -1,11 +1,9 @@
 import { combineReducers } from 'redux';
 
-import myWatchingRepos from './myWatchingRepos';
 import searchedRepos from './searchedRepos';
 import userInfo from './userInfo';
 
 const reducer = combineReducers({
-  myWatchingRepos,
   searchedRepos,
   userInfo
 });
